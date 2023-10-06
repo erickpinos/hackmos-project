468040a3a0f5560714949d1a4003604043958cd8
Erick Pinos <erick@erickpinos.com>
2023-10-07 01:52:36 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import {
+    IncentivizedTestnet,
+    NibiruQueryClient,
+    NibiruSigningClient,
+  } from "@nibiruchain/nibijs"
+  
+  const TEST_CHAIN = IncentivizedTestnet(3)
+  const queryClient = await NibiruQueryClient.connect(TEST_CHAIN.endptTm)
+  
+  const address = "nibi17qgdh87ln2wf6a6zzae25tkze6yfa2jdsywawn"
+
+  console.log("\n perpFunctions: %o", queryClient.nibiruExtensions.perp)
+  console.log("\n perpPositions: %o", await queryClient.nibiruExtensions.perp.positions({trader: address}))
+  console.log("\n allMarkets: %o", await queryClient.nibiruExtensions.perp.markets())
+
+  const signingClient = await NibiruSigningClient.connect(TEST_CHAIN.endptTm)
+  const allBalances = await signingClient.getAllBalances(address)
+  console.log("allBalances: %o", allBalances)
+
+//  const perpParamsResp = await queryClient.nibiruExtensions.perp.params()
+//  console.log("perpParams: %o", perpParamsResp)
+  
+//  const allMarkets = await queryClient.nibiruExtensions.perp.markets({
+//    pair: "ueth:unusd",
+//  })
+//  console.log("allMarkets: %o", allMarkets)
+  
+  const blockHeight = 150000
+  const block = await queryClient.getBlock(blockHeight)
+  console.log("\n allMarkets: %o", block)
